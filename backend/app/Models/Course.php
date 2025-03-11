f03495ffe361ad489bb93fb33337846567836310
Algoca05 <alejandrogomcap@campus.monlau.com>
2025-03-11 20:16:25 +0100

    first commit

---
+++ backend/app/Models/Course.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'course_user')->withPivot('role');
+    }
+
+    public function assignments()
+    {
+        return $this->hasMany(Assignment::class);
+    }
+
+    public function attendances()
+    {
+        return $this->hasMany(Attendance::class);
+    }
+}
